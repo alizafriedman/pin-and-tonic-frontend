adfe9a803435ff2f519aa4b04457b74c6b46409f
alizafriedman <alizafriedman123@gmail.com>
2020-07-01 12:53:01 -0400

    fdsg

--- pin/src/components/Boards.js
+++ pin/src/components/Boards.js
@@ -104,13 +104,13 @@ export default function Boards() {
           {context.boards.map((board) => {
             return (
               <Link to={`/boards/${board.id}`} style={{ textDecoration: "none" }}>
-                <GridList cellHeight={160} className={classes.gridList} cols={3}>
+                {/* <GridList cellHeight={160} className={classes.gridList} cols={3}>
                   {tileData.map((tile) => (
                     <GridListTile key={tile.img} cols={tile.cols || 1}>
                       <img src={tile.img} alt={tile.title} />
                     </GridListTile>
                   ))}
-                </GridList>
+                </GridList> */}
               {/* <Card className={classes.root} key={board.id}>
                 <CardActionArea >
                 <CardHeader
