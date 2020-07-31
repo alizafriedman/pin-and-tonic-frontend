32abf800f067368f78de9a75380bd98a6a5f5db9
alizafriedman <alizafriedman123@gmail.com>
2020-07-31 19:37:36 -0400

    issues

--- pin/src/config.js
+++ pin/src/config.js
@@ -1,1 +1,1 @@
-export const backendUrl = process.env.NODE_ENV == "development" ? process.env.REACT_APP_BACKENDURL : "https://metric-keener-41249.herokuapp.com"
+export const backendUrl = process.env.NODE_ENV == "development" ? process.env.REACT_APP_BACKENDURL : "https://metric-keener-41249.herokuapp.com/"
