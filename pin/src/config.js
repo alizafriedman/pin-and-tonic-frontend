3385a3826588079d8cd41c17530ca25f4eb775c6
alizafriedman <alizafriedman123@gmail.com>
2020-08-09 21:08:51 -0400

    test

--- pin/src/config.js
+++ pin/src/config.js
@@ -1,4 +1,4 @@
 export const backendUrl =
          process.env.NODE_ENV == "development"
            ? process.env.REACT_APP_BACKENDURL
-           : "https://agile-sierra-79030.herokuapp.com/";
+           : "https://agile-sierra-79030.herokuapp.com";
